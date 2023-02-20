c493192512a769b9ff115a5d3bf42d3a46b4b7d8
daniellykammers <dany.kammers@hotmail.com>
2023-02-20 15:00:27 -0300

    feat(M1S04): preparando o ambiente para a semana

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const valueInput = document.getElementById('value-input');
+const descriptionInput = document.getElementById('description-input');
+
 var mesAtual = 'janeiro';
 var entradas = 8000;
 var saidas = 400;
@@ -41,9 +44,46 @@ document.getElementById('total-expenses').innerHTML = saidas;
 // Saldo
 document.getElementById('final-balance').innerHTML = saldo;
 
+function addFinancialMovement() {
+  const earningRadioValueIsChecked =
+    document.getElementById('earning-radio').checked;
+
+  if (earningRadioValueIsChecked) {
+    addEarning();
+  } else {
+    addExpense();
+  }
+}
+
+function addExpense() {
+  const newExpense = {
+    description: descriptionInput.value,
+    value: valueInput.value,
+  };
+  expensesList.push(newExpense);
+
+  var li = document.createElement('li');
+  li.innerText = `${newExpense.description}: R$ ${newExpense.value}`;
+
+  document.getElementById('expenses-list').appendChild(li);
+}
+
+function addEarning() {
+  const newExpense = {
+    description: descriptionInput.value,
+    value: valueInput.value,
+  };
+  expensesList.push(newExpense);
+
+  var li = document.createElement('li');
+  li.innerText = `${newExpense.description}: R$ ${newExpense.value}`;
+
+  document.getElementById('earnings-list').appendChild(li);
+}
+
 for (var i = 0; i < earningsList.length; i++) {
   var li = document.createElement('li');
-  li.innerText = `${earningsList[i].description}: R$ ${expensesList[i].value}`;
+  li.innerText = `${earningsList[i].description}: R$ ${earningsList[i].value}`;
 
   document.getElementById('earnings-list').appendChild(li);
 }
